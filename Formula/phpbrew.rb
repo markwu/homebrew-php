3fcebe4f1555dd6c0a671554d2f3f4224a319c53
Mark Wu <wuhsinyee@hotmail.com>
2014-08-23 00:15:33 +0800

    Need to unlink first

--- Formula/phpbrew.rb
+++ Formula/phpbrew.rb
@@ -23,6 +23,7 @@ class Phpbrew < Formula
   depends_on "libevent"
 
   def install
+    system "brew unlink icu4c"
     system "brew link icu4c --force"
     system "chmod a+x phpbrew"
     system "mkdir -p #{prefix}/bin"
