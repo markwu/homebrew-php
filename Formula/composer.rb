971665af43fd2e3cee2e218856ada19592bf75cd
Mark Wu <wuhsinyee@hotmail.com>
2014-12-09 14:05:48 +0800

    Upgrade composer to 1.0.0 alpha 9

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -3,9 +3,9 @@ require "formula"
 class Composer < Formula
   homepage "http://getcomposer.org"
   head "https://getcomposer.org/composer.phar"
-  url "https://getcomposer.org/download/1.0.0-alpha8/composer.phar"
-  sha1 "6eefa41101a2d1a424c3d231a1f202dfe6f09cf8"
-  version "1.0.0-alpha8"
+  url "https://getcomposer.org/download/1.0.0-alpha9/composer.phar"
+  sha1 "c0917158e84c867ce69753fcbc04eb7c45bb58bb"
+  version "1.0.0-alpha9"
 
   def install
     libexec.install "composer.phar"
