a0bf825ae205799ccf1d43ec4fd765a0ff9633b4
Mark Wu <wuhsinyee@hotmail.com>
2016-04-25 22:43:32 +0800

    Add betterphp-ctags from b4n

---
+++ Formula/betterphp-ctags.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class BetterPhpCtags < Formula
+  homepage 'http://ctags.sourceforge.net/'
+  head 'https://github.com/b4n/ctags.git', :branch => 'better-php-parser'
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-macro-patterns",
+                          "--mandir=#{man}",
+                          "--with-readlib"
+    system "make install"
+  end
+
+  def caveats
+    <<-EOS.undent
+      Under some circumstances, emacs and ctags can conflict. By default,
+      emacs provides an executable `ctags` that would conflict with the
+      executable of the same name that ctags provides. To prevent this,
+      Homebrew removes the emacs `ctags` and its manpage before linking.
+
+      However, if you install emacs with the `--keep-ctags` option, then
+      the `ctags` emacs provides will not be removed. In that case, you
+      won't be able to install ctags successfully. It will build but not
+      link.
+    EOS
+  end
+end
