c8c049a2befebb324609aced35b22a03a0a174cc
Mark Wu <wuhsinyee@hotmail.com>
2016-04-26 14:14:12 +0800

    Update betterphp-ctags formula

--- Formula/betterphp-ctags.rb
+++ Formula/betterphp-ctags.rb
@@ -4,9 +4,13 @@ class BetterphpCtags < Formula
   homepage 'http://ctags.sourceforge.net/'
   head 'https://github.com/b4n/ctags.git', :branch => 'better-php-parser'
 
+  depends_on "autoconf"
+
+  patch :DATA
+
   def install
-    system "autoheader"
     system "autoconf"
+    system "autoheader"
     system "./configure", "--prefix=#{prefix}",
                           "--enable-macro-patterns",
                           "--mandir=#{man}",
@@ -28,3 +32,41 @@ class BetterphpCtags < Formula
     EOS
   end
 end
+
+__END__
+diff -ur a/read.c b/read.c
+--- a/read.c	2009-07-04 17:29:02.000000000 +1200
++++ b/read.c	2012-11-04 16:19:27.000000000 +1300
+@@ -18,7 +18,6 @@
+ #include <string.h>
+ #include <ctype.h>
+ 
+-#define FILE_WRITE
+ #include "read.h"
+ #include "debug.h"
+ #include "entry.h"
+diff -ur a/read.h b/read.h
+--- a/read.h	2008-04-30 13:45:57.000000000 +1200
++++ b/read.h	2012-11-04 16:19:18.000000000 +1300
+@@ -11,12 +11,6 @@
+ #ifndef _READ_H
+ #define _READ_H
+ 
+-#if defined(FILE_WRITE) || defined(VAXC)
+-# define CONST_FILE
+-#else
+-# define CONST_FILE const
+-#endif
+-
+ /*
+ *   INCLUDE FILES
+ */
+@@ -95,7 +89,7 @@
+ /*
+ *   GLOBAL VARIABLES
+ */
+-extern CONST_FILE inputFile File;
++extern inputFile File;
+ 
+ /*
+ *   FUNCTION PROTOTYPES
