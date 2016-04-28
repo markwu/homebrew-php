be06418dac939422db2db34431de024a7a8ca47a
Mark Wu <wuhsinyee@hotmail.com>
2016-04-28 22:33:22 +0800

    Change version string

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -5,7 +5,7 @@ class Composer < Formula
   head "https://getcomposer.org/composer.phar"
   url "https://getcomposer.org/download/1.0.2/composer.phar"
   sha256 "264673ccee900b22192605b8c74ecb77c45a5197347edacd142699866c478f4c"
-  version "1.0.0"
+  version "1.0.2"
 
   def install
     libexec.install "composer.phar"
