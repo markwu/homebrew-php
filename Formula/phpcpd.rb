d847b04436ad4025894a6e2ac01bae2a44040921
Mark Wu <wuhsinyee@hotmail.com>
2016-04-30 00:02:40 +0800

    Use specfic version download for phpcpd & phploc

--- Formula/phpcpd.rb
+++ Formula/phpcpd.rb
@@ -3,11 +3,12 @@ require "formula"
 class Phpcpd < Formula
   desc "Copy/Paste Detector (CPD) for PHP code"
   homepage "https://github.com/sebastianbergmann/phpcpd"
-  url "https://phar.phpunit.de/phpcpd.phar"
+  url "https://phar.phpunit.de/phpcpd-2.0.4.phar"
   sha256 "491eeac71f1421395648ff079b60fe4858217b70d66eaa07644351f2699c38fe"
   version "2.0.4"
 
   def install
+    File.rename("phpcpd-2.0.4.phar", "phpcpd.phar")
     libexec.install "phpcpd.phar"
     (libexec/"phpcpd.phar").chmod 0755
     bin.install_symlink libexec/"phpcpd.phar" => "phpcpd"
--- Formula/phploc.rb
+++ Formula/phploc.rb
@@ -3,11 +3,12 @@ require "formula"
 class Phploc < Formula
   desc "A tool for quickly measuring the size of a PHP project"
   homepage "https://github.com/sebastianbergmann/phploc"
-  url "https://phar.phpunit.de/phploc.phar"
+  url "https://phar.phpunit.de/phploc-3.0.1.phar"
   sha256 "a712dec6b1044505a411d207813c6b11cc1c138c0ed467f65788b6f9441c9701"
   version "3.0.1"
 
   def install
+    File.rename("phploc-3.0.1.phar", "phploc.phar")
     libexec.install "phploc.phar"
     (libexec/"phploc.phar").chmod 0755
     bin.install_symlink libexec/"phploc.phar" => "phploc"
