2f91198f25fe80d4d71c773f80c4f31fd579154f
Mark Wu <wuhsinyee@hotmail.com>
2016-05-15 23:28:24 +0800

    Add GNU global depends on betterphp-ctags

---
+++ Formula/betterphp-global.rb
@@ -0,0 +1,66 @@
+require 'formula'
+
+class BetterphpGlobal < Formula
+  desc "Source code tag system"
+  homepage "https://www.gnu.org/software/global/"
+  url "http://ftpmirror.gnu.org/global/global-6.5.4.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/global/global-6.5.4.tar.gz"
+  sha256 "af16e0a686a46f759156cb685e25f345680703f43f93af1ce8d834caaf541da6"
+
+  head do
+    url ":pserver:anonymous:@cvs.savannah.gnu.org:/sources/global", :using => :cvs
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  option "with-ctags", "Enable Exuberant Ctags as a plug-in parser"
+  option "with-pygments", "Enable Pygments as a plug-in parser (should enable exuberent-ctags too)"
+  option "with-sqlite3", "Use SQLite3 API instead of BSD/DB API for making tag files"
+
+  depends_on "markwu/php/betterphp-ctags" => :optional
+
+  skip_clean "lib/gtags"
+
+  resource "pygments" do
+    url "https://pypi.python.org/packages/source/P/Pygments/Pygments-2.1.tar.gz"
+    sha256 "13a0ef5fafd7b16cf995bc28fe7aab0780dab1b2fda0fc89e033709af8b8a47b"
+  end
+
+  def install
+    system "sh", "reconf.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--with-sqlite3" if build.with? "sqlite3"
+
+    if build.with? "ctags"
+      args << "--with-exuberant-ctags=#{Formula["markwu/php/ctags"].opt_bin}/ctags"
+    end
+
+    if build.with? "pygments"
+      ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
+      pygments_args = %W[build install --prefix=#{libexec}]
+      resource("pygments").stage { system "python", "setup.py", *pygments_args }
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    if build.with? "pygments"
+      bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    end
+
+    etc.install "gtags.conf"
+
+    # we copy these in already
+    cd share/"gtags" do
+      rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
+    end
+  end
+end
