77704761a50e33d7023df9425ed15155bd2b933c
Mark Wu <wuhsinyee@hotmail.com>
2016-08-02 23:36:53 +0800

    Upgrade phpcs to 2.6.2

--- Formula/phpcs.rb
+++ Formula/phpcs.rb
@@ -3,13 +3,13 @@ require "formula"
 class Phpcs < Formula
   desc "Check coding standards in PHP, JavaScript and CSS"
   homepage "https://github.com/squizlabs/PHP_CodeSniffer"
-  url "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/2.6.1/phpcs.phar"
-  sha256 "cfa9d7e670682aafdaef4be7c8e1a3f01440f784a8e350a655114070339a6255"
-  version "2.6.1"
+  url "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/2.6.2/phpcs.phar"
+  sha256 "8ca80f20f5cf23632d4d9f9671f87a9a9dc098966d7c7eeeafa1b59ac156a039"
+  version "2.6.2"
 
   resource "phpcbf" do
-    url "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/2.6.1/phpcbf.phar"
-    sha256 "b381189022a2bb020bd927b1e3fd1d4b2f644742fe78d44ca8b4856b2445b606"
+    url "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/2.6.2/phpcbf.phar"
+    sha256 "8149a0d23ff82c656018426e0e70374ffe4fdfbaf62228b3a4272a7ceb7e0774"
   end
 
   def install
