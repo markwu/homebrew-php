b663fb141ae70c974505527d55dadbbec4974717
Mark Wu <wuhsinyee@hotmail.com>
2016-08-02 23:43:29 +0800

    Upgrade phpunite to 5.4.8

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -3,12 +3,12 @@ require "formula"
 class Phpunit < Formula
   desc "A programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de/"
-  url "https://phar.phpunit.de/phpunit-5.4.6.phar"
-  sha256 "f1aa125103a91f573ed43bb8fea42b5e087e5d67d570e84cb4ab2215a40ecbf8"
-  version "5.4.6"
+  url "https://phar.phpunit.de/phpunit-5.4.8.phar"
+  sha256 "7f6705f9281b5c2bc1ef2c24c8fc6d26a709ee29c5c1d1c8e600bee40f0c257c"
+  version "5.4.8"
 
   def install
-    File.rename("phpunit-5.4.6.phar", "phpunit.phar")
+    File.rename("phpunit-5.4.8.phar", "phpunit.phar")
     libexec.install "phpunit.phar"
     (libexec/"phpunit.phar").chmod 0755
     bin.install_symlink libexec/"phpunit.phar" => "phpunit"
