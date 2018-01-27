afbf5006a0215de050a62d26db0a9be56f0ab6ee
Mark Wu <wuhsinyee@hotmail.com>
2018-01-27 18:17:46 +0800

    update composer to 1.6.2

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -3,9 +3,9 @@ require "formula"
 class Composer < Formula
   desc "Dependency Manager for PHP"
   homepage "http://getcomposer.org"
-  url "https://getcomposer.org/download/1.5.2/composer.phar"
-  sha256 "c0a5519c768ef854913206d45bd360efc2eb4a3e6eb1e1c7d0a4b5e0d3bbb31f"
-  version "1.5.2"
+  url "https://getcomposer.org/download/1.6.2/composer.phar"
+  sha256 "6ec386528e64186dfe4e3a68a4be57992f931459209fd3d45dde64f5efb25276"
+  version "1.6.2"
 
   def install
     libexec.install "composer.phar"
