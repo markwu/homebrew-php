c8e7b4156261e79bce48812c3d5d6875c3ce7bf7
Mark Wu <wuhsinyee@hotmail.com>
2018-02-26 23:28:38 +0800

    Support global by universal-ctags

---
+++ Formula/universal-global.rb
@@ -0,0 +1,70 @@
+require 'formula'
+
+class UniversalGlobal < Formula
+  desc "Source code tag system"
+  homepage "https://www.gnu.org/software/global/"
+  url "https://ftp.gnu.org/gnu/global/global-6.6.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/global/global-6.6.2.tar.gz"
+  sha256 "43c64711301c2caf40dc56d7b91dd03d2b882a31fa31812bf20de0c8fb2e717f"
+  revision 1
+
+  head do
+    url ":pserver:anonymous:@cvs.savannah.gnu.org:/sources/global", :using => :cvs
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "bison" => :build
+    depends_on "flex" => :build
+    ## gperf is provided by OSX Command Line Tools.
+    depends_on "libtool" => :build
+  end
+
+  option "with-ctags", "Enable Universal Ctags as a plug-in parser"
+  option "with-pygments", "Enable Pygments as a plug-in parser (should enable universal-ctags too)"
+  option "with-sqlite3", "Use SQLite3 API instead of BSD/DB API for making tag files"
+
+  depends_on "universal-ctags/universal-ctags/universal-ctags" => :optional
+
+  skip_clean "lib/gtags"
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/71/2a/2e4e77803a8bd6408a2903340ac498cb0a2181811af7c9ec92cb70b0308a/Pygments-2.2.0.tar.gz"
+    sha256 "dbae1046def0efb574852fab9e90209b23f556367b5a320c0bcb871c77c3e8cc"
+  end
+
+  def install
+    system "sh", "reconf.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--with-sqlite3" if build.with? "sqlite3"
+
+    if build.with? "ctags"
+      args << "--with-ctags=#{Formula["universal-ctags/universal-ctags/universal-ctags"].opt_bin}/ctags"
+    end
+
+    if build.with? "pygments"
+      ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+      pygments_args = %W[build install --prefix=#{libexec}]
+      resource("Pygments").stage { system "python", "setup.py", *pygments_args }
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    if build.with? "pygments"
+      bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    end
+
+    etc.install "gtags.conf"
+
+    # we copy these in already
+    cd share/"gtags" do
+      rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
+    end
+  end
+end
